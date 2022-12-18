b02c4b2871e210fee41c7eaea5cf87550b8eee02
phuntrrt <120866307+phuntrrt@users.noreply.github.com>
2022-12-18 09:17:27 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+$(window).load(function(){
+    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+});
+
+// HOME BACKGROUND SLIDESHOW
+$(function(){
+    jQuery(document).ready(function() {
+		$('body').backstretch([
+	 		//  "images/tm-bg-slide-1.jpg", 
+	 		//  "images/tm-bg-slide-2.jpg",
+			//  "images/tm-bg-slide-3.jpg",
+			 "images/01_bg.jpg", 
+			 "images/02_bg.jpg",
+			"images/03_bg.jpg",
+			"images/tm-bg-slide-1.jpg",
+	 			], 	{duration: 3200, fade: 1300});
+		});
+})
